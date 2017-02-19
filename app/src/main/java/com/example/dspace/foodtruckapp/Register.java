16270d8e725c960dfa953f9ce0ca5a4a5fbbda7e
rafaelcastro <rafc@seas.upenn.edu>
2017-02-18 20:34:25 -0500

    Test

--- app/src/main/java/com/example/dspace/foodtruckapp/Register.java
+++ app/src/main/java/com/example/dspace/foodtruckapp/Register.java
@@ -1,8 +1,14 @@
 package com.example.dspace.foodtruckapp;
 
+import android.util.Log;
+
 /**
  * Created by rafaelcastro on 2/18/17.
  */
 
 public class Register {
+
+    void test() {
+        Log.d("TEST", "test: ");
+    }
 }
