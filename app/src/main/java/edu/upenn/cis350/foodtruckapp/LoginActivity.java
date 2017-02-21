8c9aba94430b2bc1e80190ea95e034be802196f4
desmondhow <desmondhow.dh@gmail.com>
2017-02-21 10:54:04 -0500

    Add Login Activity to Manifest.

--- app/src/main/java/edu/upenn/cis350/foodtruckapp/LoginActivity.java
+++ app/src/main/java/edu/upenn/cis350/foodtruckapp/LoginActivity.java
@@ -1,13 +1,11 @@
 package edu.upenn.cis350.foodtruckapp;
 
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by rafaelcastro on 2/20/17.
  */
 
-public class LoginActivity {
-
-
+public class LoginActivity extends AppCompatActivity {
 
 }
--- app/src/main/java/edu/upenn/cis350/foodtruckapp/RegistrationActivity.java
+++ app/src/main/java/edu/upenn/cis350/foodtruckapp/RegistrationActivity.java
@@ -1,26 +1,20 @@
 package edu.upenn.cis350.foodtruckapp;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
-
-
 
 
     private EditText email;
