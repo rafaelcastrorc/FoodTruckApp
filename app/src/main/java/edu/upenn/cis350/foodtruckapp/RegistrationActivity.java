9c1ccec43360ad8f7f66634df678618bc28435e1
rafaelcastro <rafc@seas.upenn.edu>
2017-02-21 14:13:45 -0500

    Update to RegistrationActivity -Integration to Database -Errors fixed build.gradle has been changed

--- app/src/main/java/edu/upenn/cis350/foodtruckapp/RegistrationActivity.java
+++ app/src/main/java/edu/upenn/cis350/foodtruckapp/RegistrationActivity.java
@@ -18,16 +18,22 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private EditText email;
     private EditText password;
-    private FirebaseAuth mAuth;
-    private DatabaseReference mDatabase;
+    private EditText name;
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseRef;
+    private FirebaseDatabase database;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,11 +43,19 @@ public class RegistrationActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         email = (EditText) findViewById(R.id.emailField);
         password = (EditText) findViewById(R.id.passwordField);
+        name = (EditText) findViewById(R.id.nameField);
+
+        //Authentication
         mAuth = FirebaseAuth.getInstance();
+        //Database
+        database = FirebaseDatabase.getInstance();
+        databaseRef = database.getReference("Users");
+
+
     }
 
     public void btnRegistrationUser_Click(View v) {
-         // email cannot be empty
+        // email cannot be empty
         if (TextUtils.isEmpty(email.getText().toString())) {
             email.setError("This field cannot be empty");
             return;
@@ -64,20 +78,18 @@ public class RegistrationActivity extends AppCompatActivity {
                             //If user is able to register
                             Toast.makeText(RegistrationActivity.this, "Registration successful", Toast.LENGTH_LONG).show();
 
-                           // Write a message to the database
-                            FirebaseDatabase database = FirebaseDatabase.getInstance();
-                            DatabaseReference myRef = database.getReference("message");
-
-                            myRef.setValue("Hello, World!");
-
-
+                            // Write the user info to the database
+                            FirebaseUser user = task.getResult().getUser();
+                            //To represent users without clons, we use the UID
+                            String uniqueID = user.getUid();
+                            //Creates a new user
+                            writeNewUser(name.getText().toString(), uniqueID, "Costumer");
 
                             //Destination where the user should go once register successfully
                             Intent i = new Intent(RegistrationActivity.this, CustomerMainMenuActivity.class);
                             startActivity(i);
-                        }
-                        else
-                        {
+
+                        } else {
                             Log.e("There is an error", task.getException().toString());
                             Toast.makeText(RegistrationActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
                         }
@@ -88,6 +100,36 @@ public class RegistrationActivity extends AppCompatActivity {
 
     private boolean isValidPassword() {
         return false;
+    }
+
+    private void writeNewUser(String name, String uniqueName, String type) {
+        User user = new User(name, uniqueName, type);
+
+        databaseRef.child(user.dUniqueName).child("UniqueID").setValue(user.dUniqueName);
+        databaseRef.child(user.dUniqueName).child("Name").setValue(user.dName);
+        databaseRef.child(user.dUniqueName).child("Type").setValue(user.dType);
+        databaseRef.child(user.dUniqueName).child("Email").setValue(email.getText().toString());
+    }
+
+    @IgnoreExtraProperties
+    public class User {
+
+        public String dName;
+        public String dUniqueName;
+        public String dType;
+
+
+        public User() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public User(String name, String uniqueName, String typeOfUser ) {
+            this.dName = name;
+            this.dUniqueName = uniqueName;
+            this.dType = typeOfUser;
+
+        }
+
     }
 }
 
