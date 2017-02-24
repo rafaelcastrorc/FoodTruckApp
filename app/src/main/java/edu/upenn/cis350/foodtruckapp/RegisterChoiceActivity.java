29d861c430b4a131a89681cb474a330530875ded
giuseppetallini <gntallini@gmail.com>
2017-02-24 10:53:36 -0500

    added intent to register choice

--- app/src/main/java/edu/upenn/cis350/foodtruckapp/RegisterChoiceActivity.java
+++ app/src/main/java/edu/upenn/cis350/foodtruckapp/RegisterChoiceActivity.java
@@ -1,7 +1,9 @@
 package edu.upenn.cis350.foodtruckapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterChoiceActivity extends AppCompatActivity {
 
@@ -10,4 +12,16 @@ public class RegisterChoiceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_choice);
     }
+
+    public void registerVendor(View view) {
+        Intent i = new Intent(this, RegistrationVendor.class);
+        startActivity(i);
+    }
+
+    public void registerCustomer(View view) {
+        Intent i = new Intent(this, RegistrationActivity.class);
+        startActivity(i);
+    }
+
+
 }
