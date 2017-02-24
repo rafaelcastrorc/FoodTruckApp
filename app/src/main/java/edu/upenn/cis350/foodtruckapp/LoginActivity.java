98ecdfc40ee37407238399191f6a7eb7050a4d36
rafaelcastro <rafc@seas.upenn.edu>
2017-02-24 12:29:11 -0500

    Bug fix

--- app/src/main/java/edu/upenn/cis350/foodtruckapp/LoginActivity.java
+++ app/src/main/java/edu/upenn/cis350/foodtruckapp/LoginActivity.java
@@ -19,5 +19,44 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText txtEmailLogin;
+    private EditText txtPwd;
+    private FirebaseAuth firebaseAuth;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        emailLogin = (EditText) findViewById(R.id.email_field_login);
+        pswd = (EditText) findViewById(R.id.password_field_login);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    public void buttonUserLogIn(View v) {
+        final ProgressDialog progressDialog = ProgressDialog.show(LoginActivity.this, "Please wait...", "Proccessing...", true);
+
+        (firebaseAuth.signInWithEmailAndPassword(txtEmailLogin.getText().toString(), txtPwd.getText().toString()))
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+
+                        if (task.isSuccessful()) {
+                            //Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+                            //Intent i = new Intent(LoginActivity.this, ProfileActivity.class);
+                            //i.putExtra("Email", firebaseAuth.getCurrentUser().getEmail());
+                            // startActivity(i);
+                        } else {
+                            Log.e("ERROR", task.getException().toString());
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                });
+    }
+
+    public void buttonUserRegister(View v){
+        Intent i = new Intent(LoginActivity.this, RegistrationActivity.class);
+        startActivity(i);
+    }
 }
