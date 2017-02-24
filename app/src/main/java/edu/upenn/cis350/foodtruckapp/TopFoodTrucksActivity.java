19d2870d97e2137b02c849d85d370ffdc3a91e9a
desmondhow <desmondhow.dh@gmail.com>
2017-02-24 14:46:44 -0500

    Add rating bar to top food trucks page

--- app/src/main/java/edu/upenn/cis350/foodtruckapp/TopFoodTrucksActivity.java
+++ app/src/main/java/edu/upenn/cis350/foodtruckapp/TopFoodTrucksActivity.java
@@ -15,23 +15,15 @@ public class TopFoodTrucksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_food_trucks);
         ListView list = (ListView) findViewById(R.id.top_food_trucks_list);
-        String[] user_fav_trucks = {"Halal @ 38th & Walnut                             4" +
-                "", "Hemo's                                                      5",
-                "Magic Carpet                                            3",
-                "Yuh Kee's                                                  5",
-                "Mexicali                                                     2",
-                "Bubble Tea                                               5",
-                "The Real Le Ann                                      4",
-                "Bento Box                                                3",
-                "Casablanca                                             2",
-                "Bui's                                                          4"}; // data to be pulled from Firebase
+        String[] user_fav_trucks = {"Halal @ 38th & Walnut", "Hemo's", "Magic Carpet",
+                "Yuh Kee's", "Mexicali", "Bubble Tea","The Real Le Ann", }; // data to be pulled from Firebase
         list.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_style,
                 user_fav_trucks) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
                 TextView textView = ((TextView) view.findViewById(android.R.id.text1));
-                textView.setHeight(200);
+                textView.setHeight(185);
                 return view;
             }
         });
@@ -39,4 +31,6 @@ public class TopFoodTrucksActivity extends AppCompatActivity {
 
 
     }
+
+
 }
