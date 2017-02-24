cea43794a967f9021f666cbad52a8d1ef813cd68
giuseppetallini <gntallini@gmail.com>
2017-02-24 14:30:25 -0500

    added intent to register choice

--- app/src/main/java/edu/upenn/cis350/foodtruckapp/NearMeActivity.java
+++ app/src/main/java/edu/upenn/cis350/foodtruckapp/NearMeActivity.java
@@ -1,6 +1,5 @@
 package edu.upenn.cis350.foodtruckapp;
 
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class NearMeActivity extends AppCompatActivity {
 
@@ -17,10 +18,14 @@ public class NearMeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ftnear_me);
 
         ListView list = (ListView) findViewById(R.id.favs_list);
-        String[] user_fav_trucks = {"Bui's", "Hemo's", "Magic Carpet", "Yuh Kee's", "Mexicali", }; // data to be pulled from Firebase
+
+        // data to be pulled from Firebase
+        String[] user_near_trucks = {"Bui's", "Yasmin", "Hemo's", "Magic Carpet", "Yuh Kee's", "Mexicali",
+                "Magic Carpet", "Real Lee An's", "Lee An's"};
+        Arrays.sort(user_near_trucks);
+
         list.setAdapter(new ArrayAdapter<String>(this, R.layout.list_item_style,
-                user_fav_trucks) {
-            @NonNull
+                user_near_trucks) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 View view = super.getView(position, convertView, parent);
