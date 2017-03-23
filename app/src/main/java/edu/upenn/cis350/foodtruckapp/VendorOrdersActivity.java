8b7b79af8e8184420160e5e941ad5101c91c5a02
desmondhow <desmondhow.dh@gmail.com>
2017-03-23 17:02:08 -0400

    Updating previous selected order

--- app/src/main/java/edu/upenn/cis350/foodtruckapp/VendorOrdersActivity.java
+++ app/src/main/java/edu/upenn/cis350/foodtruckapp/VendorOrdersActivity.java
@@ -1,13 +1,18 @@
 package edu.upenn.cis350.foodtruckapp;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.TwoLineListItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -15,7 +20,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.ArrayList;
@@ -30,22 +34,52 @@ public class VendorOrdersActivity extends AppCompatActivity {
     DatabaseReference currentOrders;
 
     private ListView orderList;
-    private ArrayList<Order> orders = new ArrayList<>();
+    private ArrayList<Order> orders = new ArrayList<Order>();
+    private boolean isOrderSelected = false;
+    private TwoLineListItem previousChildSelected = null;
+    private Order selectedOrder = null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.food_truck_order_mgm_layout); //Todo: Create a layout for this activity
+        setContentView(R.layout.activity_vendor_orders);
+
         databaseRef = FirebaseDatabase.getInstance().getReference("Users");
         mAuth = FirebaseAuth.getInstance();
         String currVendor = mAuth.getCurrentUser().getUid();
         currentOrders = databaseRef.child(currVendor).child("Orders");
         orderList = (ListView) findViewById(R.id.order_list);
-        final ArrayAdapter<Order> arrayAdapter = new ArrayAdapter<Order>(this, android.R.layout.simple_list_item_1, orders);
+
+        final MyAdapter arrayAdapter = new MyAdapter(this, orders);
         orderList.setAdapter(arrayAdapter);
 
-        //Orders are represented in the database by a push id, and each push id has a instanceID and a message
+        // When an order is clicked by the vendor make it bold & italic
+        ListView listView = (ListView) findViewById(R.id.order_list);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                if (previousChildSelected != null) {                // set previously selected item back to normal
+                    previousChildSelected.getText1().setTypeface(null, Typeface.NORMAL);
+                    previousChildSelected.getText2().setTypeface(null, Typeface.NORMAL);
+                }
+                isOrderSelected = true;
+                // get selected order
+                final TwoLineListItem selectedChild = (TwoLineListItem) parent.getChildAt(position);
+                previousChildSelected = selectedChild;
+
+                selectedChild.getText1().setTypeface(null, Typeface.BOLD_ITALIC);
+                selectedChild.getText2().setTypeface(null, Typeface.BOLD_ITALIC);
+
+                selectedOrder = (Order) parent.getItemAtPosition(position);
+
+            }
+
+        });
+
+        // Orders are represented in the database by a push id, and each push id has a instanceID and a message
         // as children
 
         currentOrders.addChildEventListener(new ChildEventListener() {
@@ -72,9 +106,6 @@ public class VendorOrdersActivity extends AppCompatActivity {
 
                 }
                 arrayAdapter.notifyDataSetChanged();
-
-
-
 
             }
 
@@ -119,21 +150,52 @@ public class VendorOrdersActivity extends AppCompatActivity {
     }
 
 
+    // Send notification to customer that their order is ready
+    public void OrderDoneOnClick(View v) {
 
-    //Todo: Handle user cancels order, vendor rej order, vendor accepts order, vendor finish order
+        if (selectedOrder == null) {            // button clicked but no order selected
+            Toast.makeText(VendorOrdersActivity.this, "No order selected", Toast.LENGTH_LONG).show();
+            return;
+        }
 
-    //Button associated to a given user
-    public void order_done_onClick(View v) {
-
-        String idOfCostumer = "the id of the user who sent the order";
-        idOfCostumer = FirebaseInstanceId.getInstance().getId(); // for testing purposes
-        FoodTruckOrderMGM notifications = new FoodTruckOrderMGM(idOfCostumer);
+        Toast.makeText(VendorOrdersActivity.this, selectedOrder.getCostumerName() + " has been" +
+                " notified of their order!", Toast.LENGTH_LONG).show();
+        String customerInstanceId = selectedOrder.getCostumerInstanceID();
+        customerInstanceId = FirebaseInstanceId.getInstance().getId(); // for testing purposes
+        FoodTruckOrderMGM notifications = new FoodTruckOrderMGM(customerInstanceId);
         notifications.orderDone();
+
+        // make text normal
+        previousChildSelected.getText1().setTypeface(null, Typeface.NORMAL);
+        previousChildSelected.getText2().setTypeface(null, Typeface.NORMAL);
+        previousChildSelected = null;
+        isOrderSelected = false;
+    }
+
+    // Todo: Setup functionality for when a vendor cancels order
+    // Send notification to customer that their order was cancelled
+    public void OrderCancelledOnClick(View v) {
+
+        if (selectedOrder == null) {            // button clicked but no order selected
+            return;
+        }
+        Toast.makeText(VendorOrdersActivity.this, selectedOrder.getCostumerName() + "'s order" +
+                " has been cancelled!", Toast.LENGTH_LONG).show();
+        String customerInstanceId = selectedOrder.getCostumerInstanceID();
+        //customerInstanceId = FirebaseInstanceId.getInstance().getId(); // for testing purposes
+        FoodTruckOrderMGM notifications = new FoodTruckOrderMGM(customerInstanceId);
+        notifications.orderDone();
+
+        // make text normal
+        previousChildSelected.getText1().setTypeface(null, Typeface.NORMAL);
+        previousChildSelected.getText2().setTypeface(null, Typeface.NORMAL);
+        previousChildSelected = null;
+        isOrderSelected = false;
     }
 
     public class Order {
-        protected  String costumerInstanceID;
-        protected  String order;
+        protected String costumerInstanceID;
+        protected String order;
         protected String costumerName;
 
         Order(String costumerInstanceID, String order, String name ) {
@@ -142,13 +204,13 @@ public class VendorOrdersActivity extends AppCompatActivity {
             this.costumerName = name;
         }
 
-        String getCostumerInstanceID() {
+        public String getCostumerInstanceID() {
             return costumerInstanceID;
         }
-        String getCostumerOrder() {
+        public String getCostumerOrder() {
             return order;
         }
-        String getCostumername() {return costumerName;}
+        public String getCostumerName() {return costumerName;}
 
         @Override
         public boolean equals(Object o) {
@@ -168,6 +230,61 @@ public class VendorOrdersActivity extends AppCompatActivity {
             int result = costumerInstanceID != null ? costumerInstanceID.hashCode() : 0;
             result = 31 * result + (order != null ? order.hashCode() : 0);
             return result;
+        }
+
+        @Override
+        public String toString() {
+            String formattedOrder = "";
+            formattedOrder = order + "\n" + costumerName;
+            return formattedOrder;
+
+        }
+    }
+
+    class MyAdapter extends BaseAdapter {
+
+        private Context context;
+        private ArrayList<Order> orders;
+
+        public MyAdapter(Context context, ArrayList<Order> orders) {
+            this.context = context;
+            this.orders = orders;
+        }
+
+        @Override
+        public int getCount() {
+            return orders.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return orders.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            TwoLineListItem twoLineListItem;
+
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                twoLineListItem = (TwoLineListItem) inflater.inflate(
+                        android.R.layout.simple_list_item_2, null);
+            } else {
+                twoLineListItem = (TwoLineListItem) convertView;
+            }
+
+            TextView text1 = twoLineListItem.getText1();
+            TextView text2 = twoLineListItem.getText2();
+            text1.setText(orders.get(position).getCostumerName());
+            text2.setText("Halal");
+            return twoLineListItem;
         }
     }
 
